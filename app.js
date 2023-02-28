9c0e36e3a1cc195e545631231be0c4ffb6fb90c6
audisiogaston <audisiogaston@gmail.com>
2023-02-28 18:49:05 -0300

    primer desafio

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 class ProductManager {
     constructor() {
-      let products = [];
+      this.products = [];
     } 
 
     addProduct=(code,title,description,price,thumbnail,stock,products)=>{
-      this.codecode = code;
+     
+      
+      this.code = code;
       this.title = title;
       this.description = description;
       this.price = price;
       this.thumbnail = thumbnail;
       this.stock = stock;
-      products.push(product1,product2,product3);
+      const existe = this.products.some(el => el.code === parseInt(code))
+    if (existe) {
+        return existe
+      } else {
+        this.products.push({code,title,description,price,thumbnail,stock,products})
+        return console.log(this.products)
+      }
+
     }
-    getProducts(products) {
-      console.log(products)
-     }
+    /*getProducts() {
+      console.log(this.products)
+     }*/
      /*getProductById=(id)=>{
       const existe = products.some(el => el.id === parseInt(id))
       const productId = products.find( el => el.code === parseInt(id))
@@ -27,7 +36,7 @@ const product2 = new ProductManager(2,'renault clio','rojo','2000','https://www.
 const product3 = new ProductManager(3,'fiat cronos','negro','6000','https://www.google.com/url?sa=i&url=https%3A%2F%2Ffiat.fiorasisa.com.ar%2Ffiat-cronos%2F&psig=AOvVaw00tPRiOYxzHx9SbqBJPNqz&ust=1677082335390000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCICBntiAp_0CFQAAAAAdAAAAABAE://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.auto10.com%2Fsegunda-mano%2Frenault-clio-rojo%2Fq&psig=AOvVaw17-C6g8cwe1J6OIhT-wqs0&ust=1677078315722000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKjkv9vxpv0CFQAAAAAdAAAAABAJ',4);
 
 product1.addProduct()
-product1.getProducts()
+//product1.getProducts()
 //product1.getProductById(1)
 
 
