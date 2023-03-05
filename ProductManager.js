f51fb4ebebed074520eef42723b282ca6004462b
audisiogaston <audisiogaston@gmail.com>
2023-03-05 16:51:38 -0300

    segundo  desafio

---
+++ ProductManager.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+class ProductManager {
+
+    constructor(path) {
+      this.path = path;
+    }
+    addProduct = async (newProduct) => {
+      try {
+        const filenameExists = fs.existsSync(this.path);
+  
+        
+        if (!filenameExists) {
+          await fs.promises.writeFile(this.path, "[]");
+        }
+  
+        const addNewProduct = {
+          productId: Date.now(),
+          ...newProduct,
+          
+        };
+  
+        const fileContent = await fs.promises.readFile(this.path, "utf-8");
+        const fileContentParsed = JSON.parse(fileContent);
+        fileContentParsed.push(addNewProduct);
+      
+        await fs.promises.writeFile(this.path,JSON.stringify(fileContentParsed, null, 2));
+      }
+      catch (error) {
+        throw error;
+      }
+    };
+
+//----------------------------------------------------------------
+    getProducts = async()=> {
+      const fileContent = await fs.promises.readFile(this.path, "utf-8");
+      const fileContentParsed = JSON.parse(fileContent);
+      console.log(fileContentParsed)
+    }
+//----------------------------------------------------------------
+    getProductById = async (productId) => {
+      try {
+        const fileContent = await fs.promises.readFile(this.path, "utf-8");
+        const fileContentParsed = JSON.parse(fileContent);
+
+        const productFinded = fileContentParsed.find(
+          (product) => product.productId === productId
+        );
+
+        if (!productFinded) throw new Error(`Product ${productId} Not Found!`);
+          console.log("---------getProductById----------");
+        console.log(productFinded);
+      } catch (error) {
+        throw error;
+      }
+
+      }
+//----------------------------------------------------------------
+    deleteProduct = async (productId) => {
+      try {
+        const fileContent = await fs.promises.readFile(this.path, "utf-8");
+        const fileContentParsed = JSON.parse(fileContent);
+        if (fileContentParsed){
+          const index = fileContentParsed.findIndex((book)=>{
+            return book.productId == productId;
+          })
+
+          fileContentParsed.splice(index,1);
+          console.log(`'--------------Producto ${productId} Eliminado---------------'`);
+          console.log(fileContentParsed)
+        }
+        
+      }
+      catch (error) {
+        throw error;
+      }
+    };
+
+//----------------------------------------------------------------
+    deleteAllProducts = async()=> {
+        await fs.promises.unlink(this.path);
+        console.log('Se ha eliminado la Base de Datos');
+      }
+    }
+
+  module.exports = ProductManager;
+
---
+++ index.js
@@ -0,0 +1,56 @@
+const ProductManager = require("./ProductManager");
+
+const PATH = "./DB.json";
+
+const productManager = new ProductManager(PATH);
+
+const executeApp = async () => {
+    await productManager.addProduct({
+
+        id:3,
+        title:"Billy Summers",
+        description:"Suspenso",
+        price:7800,
+        thumbnail:"src/libros/billy.jpg",
+        stock:5,
+
+     });
+     try {
+        await productManager.getProducts();
+        await productManager.getProductById(1678045600176); 
+        await productManager.deleteProduct(1678045609987);
+       // await productManager.deleteAllProducts();
+      } catch (error) {
+        console.error(error);
+      }
+    
+};
+
+executeApp();
+
+
+
+/*
+        id:1,
+        title:"Heidi",
+        description:"Drama",
+        price:3500,
+        thumbnail:"src/libros/heidi.svg",
+        stock:3,
+*/
+/*
+        id:2,
+        title:"Frankenstein",
+        description:"Terror",
+        price:5600,
+        thumbnail:"src/libros/franken.svg",
+        stock:2,
+*/
+/*
+        id:3,
+        title:"Billy Summers",
+        description:"Suspenso",
+        price:7800,
+        thumbnail:"src/libros/billy.jpg",
+        stock:5,
+        */
