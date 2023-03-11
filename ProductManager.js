b00a92dc6610155b70672d0487a3235d1419f3e4
audisiogaston <audisiogaston@gmail.com>
2023-03-10 23:41:36 -0300

    segundo  desafio

--- ProductManager.js
+++ ProductManager.js
@@ -78,19 +78,29 @@ class ProductManager {
       }
 
 //----------------------------------------------------------------
-updateProduct = async (id,updateData) => {
-  const update = {...updateData};
+
+
+updateProduct = async (productId,updateData) => {
+
+  const update = updateData;
+    if(this.getProductById(productId)){
+      const fileContent = await fs.promises.readFile(this.path, "utf-8");
+      const fileContentParsed = JSON.parse(fileContent);
+  
+      const userPosition =  fileContentParsed.findIndex((u => u.productId === productId));
     
-    if(this.getProductById(id)){
-    const fileContent = await fs.promises.readFile(this.path, "utf-8");
-    const fileContentParsed = JSON.parse(fileContent);
-    fileContentParsed.push(update);
+    if (userPosition < 0) {
+      return console.log( "Producto no encontrado" );
+  }
+  fileContentParsed[userPosition] = update;
     await fs.promises.writeFile(this.path,JSON.stringify(fileContentParsed, null, 2));
     console.log("producto modificado");
     console.log(update);
     }
     else console.log("no se ha encontrado el producto");
 };
+
+
 
     }
   module.exports = ProductManager;
