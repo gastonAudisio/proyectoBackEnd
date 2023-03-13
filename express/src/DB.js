6ab24f8189b56ab0654f9692cde5069cf37ccebc
audisiogaston <audisiogaston@gmail.com>
2023-03-13 19:54:12 -0300

    tercer  desafio

---
+++ express/src/DB.js
@@ -0,0 +1,23 @@
+[
+  {
+    "productId": 1678045609987,
+    "id": 3,
+    "title": "Billy Summers",
+    "description": "Suspenso",
+    "price": 7800,
+    "thumbnail": "src/libros/billy.jpg",
+    "stock": 5
+  },
+  {
+    "productId": 1678045609987,
+    "id": 3,
+    "title": "Billy Summers",
+    "description": "Suspenso",
+    "price": 7800,
+    "thumbnail": "src/libros/billy.jpg",
+    "stock": 5
+  }
+]billy.jpg",
+    "stock": 5
+  }
+]
--- express/src/indexExpress.js
+++ express/src/indexExpress.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const app = express();
+const PORT = 8080;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const ProductManager = require("../../ProductManager");
-const products = "../../DB.json"
-const productManager = new ProductManager(products);
+const DB = "../../DB.json"
+const products = require(DB)
+const productManager = new ProductManager(DB);
 
-const PORT = 8080
+/*
+products.forEach(element => {
+    productManager.addProduct(element)
+});*/
 
 app.listen(PORT,()=>{
     console.log(`server run in port:${PORT}`);
 })
 
  app.get('/products', (req,res)=>{
-    
-    res.send(productManager.getProducts(products))
+    //const product =productManager.getProducts();
+    const {limit} = req.query;
+    res.json(limit ? products.slice(0,limit):products)
 })
 
 
-/*
-app.get('/:productId',async (req, res) => {
 
-    const product = await productManager.getProductById(req.params.productId)
+app.get('/products/:id', (req, res) => {
+    const product =  productManager.getProductById(Number.parseInt(req.params.id))  
 
     if (product) {
         res.send(product)
     }
     res.send({ messasge: "Producto no encontrado!!" })
-})*/
+})
