5f9be4b385b1cbd8c1022ca1b5859d448d2562ae
audisiogaston <audisiogaston@gmail.com>
2023-03-14 18:48:57 -0300

    tercer  desafio

--- express/src/indexExpress.js
+++ express/src/indexExpress.js
@@ -8,20 +8,16 @@ const DB = "../../DB.json"
 const products = require(DB)
 const productManager = new ProductManager(DB);
 
-
-
+//-------------------------------------------------------------------
 app.listen(PORT,()=>{
     console.log(`server run in port:${PORT}`);
 })
-
- app.get('/products', (req,res)=>{
-    //const product =productManager.getProducts();
+//-------------------------------------------------------------------
+app.get('/products', (req,res)=>{
     const {limit} = req.query;
     res.json(limit ? products.slice(0,limit):products)
 })
-
-
-
+//-------------------------------------------------------------------
 app.get('/products/:id', (req, res) => {
     const product =  productManager.getProductById(Number.parseInt(req.params.id))  
 
@@ -30,3 +26,8 @@ app.get('/products/:id', (req, res) => {
     }
     res.send({ messasge: "Producto no encontrado!!" })
 })
+
+
+// http://localhost:8080/products/
+
+// http://localhost:8080/products/1678045567261
