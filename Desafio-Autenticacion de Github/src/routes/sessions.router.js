a0d38861f6a6299730e8447e6006cff236477ef1
audisiogaston <audisiogaston@gmail.com>
2023-05-10 15:33:21 -0300

    agrego deleteAllPRoductsButton

--- Desafio-Autenticacion de Github/src/routes/sessions.router.js
+++ Desafio-Autenticacion de Github/src/routes/sessions.router.js
@@ -8,15 +8,87 @@ const router = Router();
 
 router.get("/github", passport.authenticate('github', {scope: ['user:email']}), async (req, res) => {});
 
+// router.get("/githubcallback", passport.authenticate('github', {failureRedirect: '/github/error'}), async (req, res) => {
+//     const user = req.user;
+//     req.session.user= {
+//         name : `${user.first_name} ${user.last_name}`,
+//         email: user.email,
+//         age: user.age
+//     };
+//     req.session.admin = true;
+//     res.redirect("/github");
+// });
+
+
+//--------------------------------------------------------------------------------
 router.get("/githubcallback", passport.authenticate('github', {failureRedirect: '/github/error'}), async (req, res) => {
+    console.log("User found to login:");
     const user = req.user;
-    req.session.user= {
-        name : `${user.first_name} ${user.last_name}`,
-        email: user.email,
-        age: user.age
-    };
-    req.session.admin = true;
-    res.redirect("/github");
+    console.log(user);
+    // Verificar si el correo electrónico es igual al del administrador
+    if (user.email === 'adminCoder@coder.com' && user.password === '$2b$10$j25iwNSa.pjPky3qkmuJHO7yIZgNH8Dp5MIpyHL9F4kmYkB3YJrt2') {
+        req.session.admin = true;
+        console.log('es admin');
+        
+        if(!user) return res.status(401).send({status:"error",error:"Incorrect credentials"});
+                // Crear carrito para el usuario
+                const cart = {
+                    cart_id: user._id,
+                    products: []
+                };
+                const cartResult = await cartModel.create(cart);
+                console.log('carrito numero ' + cartResult._id +' creado con exito');
+        req.session.user = {
+            _id: user._id,
+            name : `${user.first_name} ${user.last_name}`,
+            email: user.email,
+            age: user.age,
+            rol: "admin",
+            cart: {
+                cart_id: cartResult._id,
+                products: []
+              }
+        };
+        const uuser= req.session.user;
+        console.log(user.email + ' logueado con exito');
+        console.log(uuser._id);
+        const access_token = generateJWToken(user);
+        // console.log(access_token);
+        // res.send({ status: "success", payload: req.session.user,access_token: access_token, message: "¡Primer logueo realizado! :)" });
+        res.redirect("/github");
+    } else {
+        req.session.admin = false;
+        console.log('no es admin');
+      
+        if(!user) return res.status(401).send({status:"error",error:"Incorrect credentials"});
+        
+                 // Crear carrito para el usuario
+                 const cart = {
+                    cart_id: user._id,
+                    products: []
+                };
+                const cartResult = await cartModel.create(cart);
+                console.log('carrito numero ' + cartResult._id +' creado con exito');
+        req.session.user = {
+            _id: user._id,
+            name : `${user.first_name} ${user.last_name}`,
+            email: user.email,
+            age: user.age,
+            rol: "usuario",
+            cart: {
+                cart_id: cartResult._id,
+                products: []
+              }
+        }
+        const uuser= req.session.user;
+
+        // console.log(uuser._id);
+        // console.log(cart_id);
+        const access_token = generateJWToken(user);
+        // console.log(access_token);
+        // res.send({ status: "success", payload: req.session.user,access_token: access_token, message: "¡Primer logueo realizado! :)" });
+        res.redirect("/github");    
+    }
 });
 //-----------------------------------------------------------------------------
 router.post("/register", passport.authenticate('register', { failureRedirect: '/api/sessions/fail-register' }),
